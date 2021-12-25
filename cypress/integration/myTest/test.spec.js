211314d3dbf1f710e2114968e5aa259915e1241d
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2021-12-25 13:51:45 +0530

    First Commit

---
+++ cypress/integration/myTest/test.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+describe("Dummy Test", () => {
+  it("Test Home", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Edit src/App.js and save to reload.").should("exist");
+  });
+});
