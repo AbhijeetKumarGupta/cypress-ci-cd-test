756d0dd296299c9843e5e4cc9c7cf653a869d335
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2021-12-25 14:34:31 +0530

    added new test

--- cypress/integration/myTest/test.spec.js
+++ cypress/integration/myTest/test.spec.js
@@ -3,5 +3,6 @@ describe("Dummy Test", () => {
   it("Test Home", () => {
     cy.visit("http://localhost:3000/");
     cy.contains("Edit src/App.js and save to reload.").should("exist");
+    cy.contains("Learn React").should("exist");
   });
 });
